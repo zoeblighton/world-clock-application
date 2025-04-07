b7fe3b3ecdddff8a4c57a772e8518158a8dff218
zoeblighton <zoeblighton.seo@gmail.com>
2025-04-08 00:44:53 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,61 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const citySelect = document.getElementById("city");
-  const cities = document.querySelectorAll(".city");
+document.getElementById("city").addEventListener("change", function () {
+  const city = this.value;
 
-  function updateTime() {
-    let londonElement = document.querySelector("#london");
-    if (londonElement) {
-      let londonDateElement = londonElement.querySelector(".date");
-      let londonTimeElement = londonElement.querySelector(".time");
-      let londonTime = moment().tz("Europe/London");
-
-      londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
-      londonTimeElement.innerHTML = londonTime.format(
-        "h:mm:ss [<small>]A[</small>]"
-      );
-    }
-
-    let parisElement = document.querySelector("#paris");
-    if (parisElement) {
-      let parisDateElement = parisElement.querySelector(".date");
-      let parisTimeElement = parisElement.querySelector(".time");
-      let parisTime = moment().tz("Europe/Paris");
-
-      parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-      parisTimeElement.innerHTML = parisTime.format(
-        "h:mm:ss [<small>]A[</small>]"
-      );
-    }
-
-    let sydneyElement = document.querySelector("#sydney");
-    if (sydneyElement) {
-      let sydneyDateElement = sydneyElement.querySelector(".date");
-      let sydneyTimeElement = sydneyElement.querySelector(".time");
-      let sydneyTime = moment().tz("Australia/Sydney");
-
-      sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-      sydneyTimeElement.innerHTML = sydneyTime.format(
-        "h:mm:ss [<small>]A[</small>]"
-      );
-    }
-
-    let newYorkElement = document.querySelector("#new_york");
-    if (newYorkElement) {
-      let newYorkDateElement = newYorkElement.querySelector(".date");
-      let newYorkTimeElement = newYorkElement.querySelector(".time");
-      let newYorkTime = moment().tz("America/New_York");
-
-      newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
-      newYorkTimeElement.innerHTML = newYorkTime.format(
-        "h:mm:ss [<small>]A[</small>]"
-      );
-    }
-  }
-
-  citySelect.addEventListener("change", function () {
-    updateTime();
+  document.querySelectorAll(".city").forEach((cityDiv) => {
+    cityDiv.style.display = "none";
   });
 
-  updateTime();
-  setInterval(updateTime, 1000);
+  if (city) {
+    const cityElement = document.getElementById(
+      city.split("/")[1].toLowerCase()
+    );
+    cityElement.style.display = "block";
+
+    const currentTime = moment.tz(city);
+    cityElement.querySelector(".date").textContent =
+      currentTime.format("MMMM Do YYYY");
+    cityElement.querySelector(".time").textContent =
+      currentTime.format("h:mm:ss A");
+  }
 });
